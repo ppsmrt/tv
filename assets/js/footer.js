343c674c7f8e6e91f72d87bfbf7d1e06b2a72c60
Palani <124156458+ppsmrt@users.noreply.github.com>
2025-09-04 01:07:06 +0530

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,8 +1,19 @@
-// Footer navigation buttons
-const footer = document.querySelector('footer');
-footer.innerHTML = `
-  <button class="bottom-nav-btn active" onclick="window.location.href='index.html'"><span class="material-icons">home</span>Home</button>
-  <button class="bottom-nav-btn"><span class="material-icons">live_tv</span>Live</button>
-  <button class="bottom-nav-btn"><span class="material-icons">search</span>Search</button>
-  <button class="bottom-nav-btn" onclick="window.location.href='signin.html'"><span class="material-icons">person</span>Profile</button>
+// footer.js
+const footerHTML = `
+<footer class="fixed bottom-0 w-full flex justify-around bg-black bg-opacity-60 p-2">
+  <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500" onclick="window.location.href='index.html'">
+    <span class="material-icons">home</span>Home
+  </button>
+  <button class="flex flex-col items-center text-sm text-red-500">
+    <span class="material-icons">live_tv</span>Live
+  </button>
+  <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500">
+    <span class="material-icons">search</span>Search
+  </button>
+  <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500" onclick="window.location.href='signin.html'">
+    <span class="material-icons">person</span>Profile
+  </button>
+</footer>
 `;
+
+document.body.insertAdjacentHTML('beforeend', footerHTML);
