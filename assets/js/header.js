0860f12d1f79302aaa28b90edb2de73f90e61edc
Palani <124156458+ppsmrt@users.noreply.github.com>
2025-09-04 11:13:06 +0530

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,9 @@
 // header.js
 export function injectHeader() {
   const headerHTML = `
-    <header>
-      <span class="material-icons">live_tv</span>
-      <h1 class="ml-2 text-lg font-bold">Live Player</h1>
+    <header class="flex items-center px-6 h-16 bg-gray-900/90 shadow-md z-50">
+      <span class="material-icons text-white">live_tv</span>
+      <h1 class="ml-2 text-lg font-bold text-white">Live Player</h1>
     </header>
   `;
   document.body.insertAdjacentHTML('afterbegin', headerHTML);
