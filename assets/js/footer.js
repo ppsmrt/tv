36190227e664f667b02244a8cc7c6d7f3c26bcd4
Palani <124156458+ppsmrt@users.noreply.github.com>
2025-09-05 23:21:56 +0530

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,16 +1,16 @@
 // footer.js
 const footerHTML = `
 <footer class="fixed bottom-0 w-full flex justify-around bg-black bg-opacity-60 p-2">
-  <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500" data-href="index.html">
+  <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500" data-href="index">
     <span class="material-icons">home</span>Home
   </button>
   <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500" data-href="generate">
     <span class="material-icons">subscriptions</span>Playlist
   </button>
-  <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500" data-href="search.html">
+  <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500" data-href="search">
     <span class="material-icons">search</span>Search
   </button>
-  <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500" data-href="signin.html">
+  <button class="flex flex-col items-center text-sm text-gray-300 hover:text-red-500" data-href="signin">
     <span class="material-icons">person</span>Profile
   </button>
 </footer>
